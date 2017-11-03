7f5e549983c69063aa6fe9244e6f8c481b62dae5
DaniMangtani <DaniMangtani@10.230.175.109>
2017-11-03 15:54:43 +0000

    Kata 4 version 2

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -1,5 +1,6 @@
 package kata4;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import pa04.model.Histogram;
@@ -9,12 +10,33 @@ import pa04.view.MailHistogramBuilder;
 import pa04.view.MailListReader;
 
 public class Kata4 {
-
-    public static void main(String[] args) throws IOException {
-        String fileName = "/Users/DaniMangtani/NetBeansProjects/Kata4/emails.txt";
-        List<Mail> mailList = MailListReader.read(fileName);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
+    public static void main(String[] args) throws IOException, Exception {
+        Kata4 histo = new Kata4();
+        histo.execute();
+    }
+    
+    private String filename;
+    private List<Mail> mailList;
+    private Histogram<String> histogram;
+    private static HistogramDisplay histoDisplay;
+        
+    private void execute() throws Exception{
+        input();
+        process();
+        output();
+    }
+    
+    private void input() throws IOException{
+        filename = "/Users/DaniMangtani/NetBeansProjects/Kata4/emails.txt";
+        mailList = MailListReader.read(filename);
+    }
+    
+    private void process() throws Exception{
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+    
+    private void output(){
+        histoDisplay = new HistogramDisplay(histogram);
         histoDisplay.execute();
     }
 }
